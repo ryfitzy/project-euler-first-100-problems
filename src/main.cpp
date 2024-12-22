f2b59f483fa12865d85e860e2f0918a3b759a52b
Ryan Fitzgerald <sit28@txstate.edu>
2024-12-21 22:54:58 -0600

    Backing out of making the project with CMake

--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "iostream"
-#include "Problem1.h"
-
-int main() {
-    std::cout << Problem1::solution() << std::endl;
-    return 0;
-}
--- src/problems/Problem1.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROBLEM1_H  // Check if PROBLEM1_H is not defined
-#define PROBLEM1_H  // Define PROBLEM1_H
-
-// Content of the header file goes here
-namespace Problem1 {
-    int solution();
-}
-
-#endif // End of the conditional block
