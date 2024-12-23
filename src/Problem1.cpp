a8cfc123fc649c1ad793dde62fc2afa84186f708
Ryan Fitzgerald <sit28@txstate.edu>
2024-12-22 21:25:01 -0600

    Problem 2 solved

--- src/Problem1.cpp
+++ src/Problem1.cpp
@@ -4,17 +4,14 @@
 
 int Problem1::solution() {
     // A generalized approach solved in O(1) time using Gauss's Formula
-    int upperBound = 1000;
-    int multiple1 = 3;
-    int multiple2 = 5;
-    int multiple3 = multiple1*multiple2;
+    unsigned int upperBound = 1000, multiple1 = 3, multiple2 = 5, multiple3 = multiple1*multiple2;
 
     // Ensures to exclude the upper bound by subtracting one of the multiple bound of it divides the upper bound
-    int multiBound1 = upperBound % multiple1 == 0 ? upperBound / multiple1 - 1 : upperBound / multiple1;
-    int multiBound2 = upperBound % multiple2 == 0 ? upperBound / multiple2 - 1 : upperBound / multiple2;
-    int multiBound3 = upperBound % multiple3 == 0 ? upperBound / multiple3 - 1 : upperBound / multiple3;
+    unsigned int multiBound1 = upperBound % multiple1 == 0 ? upperBound / multiple1 - 1 : upperBound / multiple1;
+    unsigned int multiBound2 = upperBound % multiple2 == 0 ? upperBound / multiple2 - 1 : upperBound / multiple2;
+    unsigned int multiBound3 = upperBound % multiple3 == 0 ? upperBound / multiple3 - 1 : upperBound / multiple3;
 
-    // Apply Gauss's Formula
+    // Apply Gauss's Formula to retrieve the sum of all multiples of 3 and 5 then subtract off their intersection
     return multiple1*(multiBound1 * (multiBound1 + 1) / 2) + 
            multiple2*(multiBound2 * (multiBound2 + 1) / 2) - 
            multiple3*(multiBound3 * (multiBound3 + 1) / 2);
--- src/Problem2.cpp
+++ src/Problem2.cpp
@@ -1,5 +1,15 @@
 #include "Problems.h"
 
+// Problem 2 Description Link: https://projecteuler.net/problem=2
+
 int Problem2::solution() {
-    return 0;
+    // Generic solution development
+    unsigned int sum = 2, upperBound = 4000000, x = 1, y = 2;
+    while (y < upperBound) {
+        unsigned int next = x + y;
+        if (next % 2 == 0) sum += next;
+        x = y;
+        y = next;
+    }
+    return sum;
 }
