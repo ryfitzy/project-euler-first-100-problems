71e2e75267610073a61541dded8a2e0402d041c8
Ryan Fitzgerald <sit28@txstate.edu>
2024-12-24 15:17:54 -0600

    Solved Problem 6

---
+++ include/problem6.h
@@ -0,0 +1,8 @@
+#ifndef PROBLEM6
+#define PROBLEM6
+
+namespace Problem6 {
+    int solution();
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <cstdio>
 #include <sys/time.h>
-#include <problem5.h>
+#include <problem6.h>
 
 int main(int, char**) {
     // start time
@@ -8,7 +8,7 @@ int main(int, char**) {
     gettimeofday(&beg, nullptr);
 
     // execute timed code section (change this line for different problems)
-    int ans = Problem5::solution();
+    int ans = Problem6::solution();
 
     // stop time
     gettimeofday(&end, nullptr);
