4baa39ccccbee2ab6dd5a86bc10ad855f8a22990
Ryan Fitzgerald <sit28@txstate.edu>
2024-12-25 23:19:40 -0600

    Added source/header file for Problem 9

---
+++ include/problem9.h
@@ -0,0 +1,8 @@
+#ifndef PROBLEM9
+#define PROBLEM9
+
+namespace Problem9 {
+    unsigned long long solution();
+}
+
+#endif
---
+++ src/problem9.cpp
@@ -0,0 +1,11 @@
+#include "problem8.h"
+#include "toolkit.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using ull = unsigned long long;
+
+ull Problem8::solution() {
+    return 0;
+}
